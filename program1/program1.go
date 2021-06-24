922c8a3bc4f92538d30c7ac3454414937f67c89f
Atsuya Ueta <m215670@hiroshima-u.ac.jp>
2021-06-24 05:31:18 +0000

    modify file

--- program1/program1.go
+++ program1/program1.go
@@ -38,7 +38,67 @@ func numtoroma(num string, i int) string {
 			roma = "VIII"
 		case "9":
 			roma = "IX"
-
+		default:
+			roma = ""
+		}
+	} else if i == 2 {
+		switch num {
+		case "1":
+			roma = "X"
+		case "2":
+			roma = "XX"
+		case "3":
+			roma = "XXX"
+		case "4":
+			roma = "XL"
+		case "5":
+			roma = "L"
+		case "6":
+			roma = "LX"
+		case "7":
+			roma = "LXX"
+		case "8":
+			roma = "LXXX"
+		case "9":
+			roma = "XC"
+		default:
+			roma = ""
+		}
+	} else if i == 3 {
+		switch num {
+		case "1":
+			roma = "C"
+		case "2":
+			roma = "CC"
+		case "3":
+			roma = "CCC"
+		case "4":
+			roma = "CD"
+		case "5":
+			roma = "D"
+		case "6":
+			roma = "DC"
+		case "7":
+			roma = "DCC"
+		case "8":
+			roma = "DCCC"
+		case "9":
+			roma = "CM"
+		default:
+			roma = ""
+		}
+	} else if i == 4 {
+		switch num {
+		case "1":
+			roma = "M"
+		case "2":
+			roma = "MM"
+		case "3":
+			roma = "MMM"
+		case "4":
+			roma = "MMMM"
+		default:
+			roma = ""
 		}
 	}
 	return roma
--- program1/program1_test.go
+++ program1/program1_test.go
@@ -2,9 +2,30 @@ package main
 
 import "testing"
 
-func Program101(t *testing.T) {
+func TestProgram101(t *testing.T) {
 	str := Program1("1")
 	if str != "I" {
 		t.Error("Test01 is failed")
+	}
+}
+
+func TestProgram102(t *testing.T) {
+	str := Program1("99")
+	if str != "XCIX" {
+		t.Error("Test02 is failed")
+	}
+}
+
+func TestProgram103(t *testing.T) {
+	str := Program1("101")
+	if str != "CI" {
+		t.Error("Test03 is failed")
+	}
+}
+
+func TestProgram104(t *testing.T) {
+	str := Program1("2008")
+	if str != "MMVIII" {
+		t.Error("Test04 is failed")
 	}
 }
